dfaf84dfa5bba14b0cb66c1b78039768d8ffa4f6
vkoenv <47176914+vkoenv@users.noreply.github.com>
2021-05-30 22:00:20 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // fill in your value for size of the observation space (sizeo), maximum time (tmax), and the other variables that make up the bionomial coefficient.
+        ObservableMaker observableMaker = new ObservableMaker(5, 1, 0.03, 0.03, 0.4, 0.7);
+        System.out.println(observableMaker.toString());
+    }
+}
